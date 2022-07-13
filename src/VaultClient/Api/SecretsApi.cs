8dfd520cc2ccfc3a9fe62121ab916dc25257d61c
Annalise Diroff <adiroff@hashicorp.com>
2022-07-13 14:01:53 -0700

    Remove unused namespaces

--- src/VaultClient/Api/SecretsApi.cs
+++ src/VaultClient/Api/SecretsApi.cs
@@ -9,12 +9,7 @@
 
 
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
 using VaultClient.Client;
