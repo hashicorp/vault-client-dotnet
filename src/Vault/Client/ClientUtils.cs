6d46b390439030df9cc57666a63633d19a9746bb
Annalise Diroff <adiroff@hashicorp.com>
2022-09-01 21:11:48 -0700

    remove duplicating headers

--- src/Vault/Client/ClientUtils.cs
+++ src/Vault/Client/ClientUtils.cs
@@ -43,11 +43,6 @@ namespace Vault.Client
                 clone.Properties.Add(prop);
             }
 
-            foreach (KeyValuePair<string, IEnumerable<string>> header in req.Headers)
-            {
-                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
-            }
-
             return clone;
         }
 
