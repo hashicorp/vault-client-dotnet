f25b9b608200bed738bd013f04630ae89cd6b9e5
Annalise Diroff <adiroff@hashicorp.com>
2022-09-06 20:42:23 -0700

    Readd request headers in

--- src/Vault/Client/ClientUtils.cs
+++ src/Vault/Client/ClientUtils.cs
@@ -39,9 +39,10 @@ namespace Vault.Client
             clone.Version = req.Version;
 
             foreach (KeyValuePair<string, object> prop in req.Properties)
-            {
                 clone.Properties.Add(prop);
-            }
+            
+            foreach (KeyValuePair<string, IEnumerable<string>> header in req.Headers)
+                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
 
             return clone;
         }
