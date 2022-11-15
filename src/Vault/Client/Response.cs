7c9025c8ea6d7a5c4fc1946ef35b0ad9acaf08d6
Annalise Diroff <adiroff@hashicorp.com>
2022-11-15 13:46:37 -0800

    Response data model

--- src/Vault/Client/Response.cs
+++ src/Vault/Client/Response.cs
@@ -6,7 +6,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Net;
+using Newtonsoft.Json;
 
 namespace Vault.Client
 {
@@ -16,43 +16,149 @@ namespace Vault.Client
     /// <typeparam name="TData">The type of the data contained in the secret.</typeparam>
     public class VaultResponse<TData>
     {
-        string RequestID;
-        string LeaseID;
-        int LeaseDuration;
-        bool Renewable;
-        IEnumerable<string> Warnings;
+        ///<summary>
+        ///</summary>
+        [JsonProperty("request_id")]
+        public string RequestID;
 
+        ///<summary>
+        ///</summary>
+        [JsonProperty("lease_id")]
+        public string LeaseID;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("lease_duration")]
+        public int LeaseDuration;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("renewable")]
+        public bool Renewable;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("warnings")]
+        public List<string> Warnings;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("data")]
         public TData Data;
 
-        ResponseAuth ResponseAuth;
-        WrapInfo WrapInfo;
+        ///<summary>
+        ///</summary>
+        [JsonProperty("auth")]
+        public ResponseAuth ResponseAuth;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("wrap_info")]
+        public ResponseWrapInfo ResponseWrapInfo;
     }
 
+    /// <summary>
+    ///
+    /// </summary>
     public class ResponseAuth
     {
+        ///<summary>
+        ///</summary>
+        [JsonProperty("client_token")]
         public string ClientToken;
-        public string Accessor;
-        List<string> Policies;
-        List<string> TokenPolicies;
-        List<string> IdentityPolicies;
-        // Metadata
-        bool orphan;
-        string EntityID;
 
-        int LeaseDuration;
-        bool Renewable;
+        ///<summary>
+        ///</summary>
+        [JsonProperty("accessor")]
+        public string Accessor;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("policies")]
+        public List<string> Policies;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("token_policies")]
+        public List<string> TokenPolicies;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("identity_policies")]
+        public List<string> IdentityPolicies;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("metadata")]
+        public Dictionary<string, string> Metadata;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("orphan")]
+        public bool orphan;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("entity_id")]
+        public string EntityID;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("lease_duration")]
+        public int LeaseDuration;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("renewable")]
+        public bool Renewable;
 
         // MFARequirement
     }
 
-    public class WrapInfo
+    /// <summary>
+    /// 
+    /// </summary>    
+    public class ResponseWrapInfo
     {
+        ///<summary>
+        ///</summary>
+        [JsonProperty("token")]
         public string Token;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("accessor")]
         public string Accessor;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("ttl")]
         public int TTL;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("creation_time")]
         public DateTime CreationTime;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("creation_path")]
         public string CreationPath;
+
+        ///<summary>
+        ///</summary>
+        [JsonProperty("wrappedAccessor")]
         public string WrappedAccessor;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>    
+    public class MFARequirement
+    {
+        ///<summary>
+        ///</summary>
+        [JsonProperty("mfa_request_id")]
+        public string MFARequestId;
+    }
 }
