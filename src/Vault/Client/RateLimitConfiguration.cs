c7ebc390397f076fa2a69a0b77bd167a04ddbdd5
Annalise Diroff <adiroff@hashicorp.com>
2023-03-07 15:25:39 -0800

    Adding header to ratelimit config

--- src/Vault/Client/RateLimitConfiguration.cs
+++ src/Vault/Client/RateLimitConfiguration.cs
@@ -1,5 +1,7 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
+//
+// Code generated with OpenAPI Generator (https://openapi-generator.tech); DO NOT EDIT.
 
 using System;
 using System.Linq;
